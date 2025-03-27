88a8bb71f09cce95fa85fb9b313bcfafe8ea5644
Ayder <muzdabaev252@gmail.com>
2025-03-27 08:08:37 +0300

    Vue_12.1

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,15 @@
-import { createStore } from "vuex";
- import ads from "./ads";
- 
- const store = createStore({
-    modules: {
-        ads,
-      },
-    });
-    export default store;
+import { createStore } from 'vuex'
+import ads from './ads'
+import user from './user'
+
+
+
+const store = createStore({
+modules:{
+ads, user
+}
+})
+
+
+
+export default store
