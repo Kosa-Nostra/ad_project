7dae735192d310fe51c4e27adb6788437c61d62a
Ayder <muzdabaev252@gmail.com>
2025-03-27 08:15:37 +0300

    Vue_13.1

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
+import shared from './shared'
 
 
 
 const store = createStore({
 modules:{
-ads, user
+ads, user, shared
 }
 })
 
