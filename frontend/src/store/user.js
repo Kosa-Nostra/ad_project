09d99481bff81d9d9844748b56ba6fc287b0934f
Ayder <muzdabaev252@gmail.com>
2025-03-27 09:59:23 +0300

    Vue_13.3

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,30 +1,30 @@
 class User {
-    constructor(id, email, password) {
-      this.id = id;
-      this.email = email;
-      this.password = password;
-    }
+  constructor(id, email, password) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
   }
-  
-  export default {
-    state: {
-      user: null,
+}
+
+export default {
+  state: {
+    user: null,
+  },
+  mutations: {
+    setUser(state, payload) {
+      console.log(payload);
+      state.user = payload;
     },
-    mutations: {
-      setUser(state, payload) {
-        console.log(payload);
-        state.user = payload;
-      },
+  },
+  actions: {
+    registerUser({ commit }, { email, password }) {
+      //Здесь запрос на сервер для регистрации
+      commit("setUser", new User(1, email, password));
     },
-    actions: {
-      registerUser({ commit }, { email, password }) {
-        //Здесь запрос на сервер для регистрации
-        commit("setUser", new User(1, email, password));
-      },
+  },
+  getters: {
+    user(state) {
+      return state.user;
     },
-    getters: {
-      user(state) {
-        return state.user;
-      },
-    },
-  };
+  },
+};
