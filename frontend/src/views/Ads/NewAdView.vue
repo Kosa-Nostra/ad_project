28af1dbc2b26268a621086924545c62f4e3e1d2b
Ayder <muzdabaev252@gmail.com>
2025-03-27 07:54:33 +0300

    Vue_11.1

--- frontend/src/views/Ads/NewAdView.vue
+++ frontend/src/views/Ads/NewAdView.vue
@@ -60,7 +60,8 @@
                  const ad = {
                      title: this.title,
                      desc: this.description,
-                     promo: this.promo
+                     promo: this.promo,
+                     src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
                  }
                  console.log(ad)
              }
